24d76542796d9b9ca9350e9ada76657418ed72f7
sagar <sagarboora.2016@gail.com>
2020-05-10 00:26:23 +0530

    Added front end part

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.use(express.urlencoded());
+const db= require("./config/mongoose");
+app.set("view engine","ejs");
+app.set("views","views");
+app.use(express.static("assets"));
 app.listen(port,function(err){
     if(err){
         console.log("Error in launching Server.");
         return;
     }
     console.log("Server Launched.");
+});
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+app.get("/getInDatabase",function(req,res){
+    console.log("Done");
+    
+    return res.redirect("/");
 });
